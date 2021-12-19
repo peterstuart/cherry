eba66fdfe283f8e7c3681ac95c50c57fed03e40c
Peter Stuart <peter@peterstuart.org>
2021-12-18 20:22:24 -0500

    Document sizing behavior

--- src/widget/mod.rs
+++ src/widget/mod.rs
@@ -5,8 +5,11 @@ use alloc::boxed::Box;
 use embedded_graphics::prelude::*;
 
 pub trait Widget<Display: DrawTarget> {
+    /// Returns the intrinsic size of the widget, based on its contents.
     fn intrinsic_size(&self) -> Size;
 
+    /// Draws the widget with the provided `origin` and `size`. If no
+    /// `size` is provided, the widget uses its [intrinsic size][Widget::intrinsic_size].
     fn draw(
         &self,
         display: &mut Display,
